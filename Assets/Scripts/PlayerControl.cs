6b539ea5406a885d066e63de8ecdfa1efd098c8c
Alejandro Medina <drolejan@gmail.com>
2022-05-13 13:14:24 -0500

    update script

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    Rigidbody2D rb;
+    float movX;
+    public float speed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        movX = Input.GetAxisRaw("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movX, 0)*speed;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Bullet"))
